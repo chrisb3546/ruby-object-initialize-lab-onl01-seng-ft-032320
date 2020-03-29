a0fd76a9f6301e1eb879f57b155ec4582ce6655b
chris brady <chrisb3546@yahoo.com>
2020-03-29 17:10:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
   
-  def initialize(name, breed = mutt)
+  def initialize(name, breed = "Mutt")
     @name = name 
     @breed = breed
   end 
@@ -9,7 +9,7 @@ class Dog
     @name=name 
   end 
   
-  def breed=(breed = Mutt)
+  def breed=(breed = "Mutt")
     @breed = breed
   end
   def name  
